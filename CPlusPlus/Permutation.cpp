0b576c24c281f060a5c080bc54a7115610a26265
AlexPham3011 <binhchauproject@gmail.com>
2017-09-16 00:56:13 +0800

    Add files via upload

---
+++ CPlusPlus/Permutation.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void Permutation(string s, string out){
+	if(s.length() == 0){
+		cout << out << "\n" ;
+	}
+	for(int i = 0; i < s.length(); i++){
+		char m = s[i];
+		string remStr = s.substr(0,i) + s.substr(i+1,s.length());
+		Permutation(remStr,out+m);
+	}
+}
+
+
+void Permutation2(string s, string out){
+	cout << "SB:" << s << endl;
+	cout << "CB:" << out << endl;
+	if (s.size() == 0){
+		cout << "YEAH:" << out << endl;
+		return;
+	}
+	
+	for (int i = 0; i < s.length(); i++){
+		
+		cout << "S:" << s.substr(1) << endl;
+		cout << "C:" << out + s[0] << endl;
+		Permutation2(s.substr(1),out + s[0]);
+		rotate(s.begin(), s.begin() + 1, s.end());
+	}
+}
+
+
+int main(){
+	string s = "AABC";
+//	Permutation(s);
+//	cout << s.substr(0) << endl;
+//	Permutation2(s, "");
+	Permutation(s,"");
+	
+}
